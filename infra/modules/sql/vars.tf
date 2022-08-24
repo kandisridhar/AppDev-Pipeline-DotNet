0cbb44471eb1963329f1e774d37e9120733224ed
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-24 21:24:14 +0530

    Create vars.tf

---
+++ infra/modules/sql/vars.tf
@@ -0,0 +1,18 @@
+variable "rg_name" {
+  default     = "ranjith"
+}
+variable "location" {
+  default     = "eastus"
+}
+variable "storage_name" {
+  default     = "dotnetcoresamplestorage"
+}
+variable "sql_server_name" {
+  default     = "dotnetcoresamplesqlserver"
+}
+variable "sql_username" {
+  default     = "sqluser"
+}
+variable "sql_password" {
+  default     = "Dotnet@2405"
+}
