63246b83f937269ec43bad1134eb22b04a8db438
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-24 21:23:40 +0530

    Update main.tf

--- infra/modules/sql/main.tf
+++ infra/modules/sql/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_storage_account" "main" {
-  name                     = "dotnetstorage"
-  resource_group_name      = "ranjith"
-  location                 = "eastus"
+  name                     = var.storage_name
+  resource_group_name      = var.rg_name
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_sql_server" "example" {
-  name                         = "dotnetcoresql"
-  resource_group_name          = "ranjith"
-  location                     = "eastus"
+resource "azurerm_sql_server" "main" {
+  name                         = var.sql_server_name
+  resource_group_name          = var.rg_name
+  location                     = var.location
   version                      = "12.0"
-  administrator_login          = "sqluser"
-  administrator_login_password = "Dotnet@2405"
+  administrator_login          = var.sql_username
+  administrator_login_password = var.sql_password
 
   tags = {
     environment = "development"
