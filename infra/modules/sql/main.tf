323306ad4bae6acdf88f4f685b2d85570973784e
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 11:48:24 +0530

    Update main.tf

--- infra/modules/sql/main.tf
+++ infra/modules/sql/main.tf
@@ -14,13 +14,6 @@ resource "azurerm_sql_server" "main" {
   administrator_login          = "sqluser"
   administrator_login_password = "Dotnet@24056"
   
-resource "azurerm_sql_firewall_rule" "main" {
-  name                = "FirewallRule1"
-  resource_group_name = var.rg_name
-  server_name         = azurerm_sql_server.main.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "255.255.255.255"
-}
 
   tags = {
     environment = "development"
